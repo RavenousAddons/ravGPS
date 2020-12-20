cdad123bf6571b390a57455c60edc1b1d885f561
waldenp0nd <20667094+waldenp0nd@users.noreply.github.com>
2020-12-20 23:08:22 +0000

    1.0.0

---
+++ Addon.lua
@@ -0,0 +1,126 @@
+---
+-- Ravenous GPS
+--   Let people know where you/your target are!
+-- Author: waldenp0nd
+-- License: Public Domain
+---
+local name, ravGPS = ...
+
+local defaults = {
+    COMMAND = "wp",
+    LOCALE =  "enUS"
+}
+
+local guild, _, _, _ = GetGuildInfo("player")
+
+local function prettyPrint(message, full)
+    if full == false then
+        message = message .. ":"
+    end
+    local prefix = "|cffff6b6b" .. ravGPS.name .. (full and " " or ":|r ")
+    print(prefix .. message)
+end
+
+local function sendVersionData()
+    C_ChatInfo.SendAddonMessage(name, RAVGPS_version, "YELL")
+    C_ChatInfo.SendAddonMessage(name, RAVGPS_version, "PARTY")
+    C_ChatInfo.SendAddonMessage(name, RAVGPS_version, "RAID")
+    if guild then
+        C_ChatInfo.SendAddonMessage(name, RAVGPS_version, "GUILD")
+    end
+end
+
+local function coordinates(a, b)
+    local channel = a and ((a:lower() == "p" or a:lower() == "party") and "PARTY" or (a:lower() == "i" or a:lower() == "instance" or a:lower() == "r" or a:lower() == "raid") and "RAID" or (a:lower() == "g" or a:lower() == "guild") and "GUILD" or (a:lower() == "o" or a:lower() == "officer") and "OFFICER" or (a:lower() == "w" or a:lower() == "whisper") and "WHISPER" or nil) or nil
+    local channelTarget = a and (b and b or 1) or playerName
+    local targetName, _ = UnitName("target")
+    local waypoint = C_Map.GetUserWaypoint()
+    local mapID = C_Map.GetBestMapForUnit("player")
+    local placedOnParent = false
+    -- Jump up parentMapIDs when unable to set a waypoint on current map
+    while (not C_Map.CanSetUserWaypointOnMap(mapID)) do
+        local mapInfo = C_Map.GetMapInfo(mapID)
+        if mapInfo.parentMapID then
+            placedOnParent = true
+            mapID = mapInfo.parentMapID
+        else
+            break
+        end
+    end
+    local coordinates = C_Map.GetPlayerMapPosition(mapID, "player")
+    if C_Map.CanSetUserWaypointOnMap(mapID) then
+        C_Map.SetUserWaypoint(UiMapPoint.CreateFromCoordinates(mapID, coordinates.x, coordinates.y))
+        if placedOnParent then
+            print(string.format(ravGPS.locales[ravGPS.locale].parentplace, ravGPS.name))
+        end
+        message = ((targetName and targetName ~= playerName) and string.format(ravGPS.locales[ravGPS.locale].messages.target, targetName) or ravGPS.locales[ravGPS.locale].messages.player) .. C_Map.GetUserWaypointHyperlink()
+        if channel then
+            SendChatMessage(message, channel, _, channelTarget)
+        else
+            print(message)
+        end
+        if waypoint then
+            C_Map.SetUserWaypoint(UiMapPoint.CreateFromCoordinates(waypoint.uiMapID, waypoint.position.x, waypoint.position.y))
+        else
+            C_Map.ClearUserWaypoint()
+        end
+    else
+        prettyPrint(ravGPS.locales[ravGPS.locale].cantplace)
+    end
+end
+
+SLASH_RAVGPS1 = "/" .. defaults.COMMAND
+SLASH_RAVGPS2 = "/ravgps"
+local function slashHandler(message, editbox)
+    local command, argument, _ = strsplit(" ", message)
+    if command == "version" or command == "v" then
+        prettyPrint(string.format(ravGPS.locales[ravGPS.locale].version, ravGPS.version))
+        sendVersionData()
+    elseif command == "help" or command == "h" then
+        prettyPrint(ravGPS.locales[ravGPS.locale].help[1])
+        print(string.format(ravGPS.locales[ravGPS.locale].help[2], defaults.COMMAND))
+        print(string.format(ravGPS.locales[ravGPS.locale].help[3], defaults.COMMAND, defaults.COMMAND))
+        print(string.format(ravGPS.locales[ravGPS.locale].help[4], ravGPS.name))
+        print(string.format(ravGPS.locales[ravGPS.locale].help[5], ravGPS.discord))
+    else
+        coordinates(command, argument)
+    end
+end
+SlashCmdList["RAVGPS"] = slashHandler
+
+local function OnEvent(self, event, arg, ...)
+    if arg == name then
+        if event == "ADDON_LOADED" then
+            ravGPS.locale = GetLocale()
+            if not ravGPS.locales[ravGPS.locale] then
+                ravGPS.locale = defaults.LOCALE
+            end
+            if not RAVGPS_version then
+                prettyPrint(string.format(ravGPS.locales[ravGPS.locale].load.install, ravGPS.name))
+            elseif RAVGPS_version ~= ravGPS.version then
+                prettyPrint(string.format(ravGPS.locales[ravGPS.locale].load.update, ravGPS.version))
+            end
+            if not RAVGPS_version or RAVGPS_version ~= ravGPS.version then
+                print(string.format(ravGPS.locales[ravGPS.locale].help[2], defaults.COMMAND))
+                print(string.format(ravGPS.locales[ravGPS.locale].help[3], defaults.COMMAND, defaults.COMMAND))
+                print(string.format(ravGPS.locales[ravGPS.locale].load.both, defaults.COMMAND))
+                RAV_seenUpdate = false
+            end
+            RAVGPS_version = ravGPS.version
+            C_ChatInfo.RegisterAddonMessagePrefix(name)
+            sendVersionData()
+        elseif event == "CHAT_MSG_ADDON" and RAVGPS_seenUpdate == false then
+            local message, _ = ...
+            local a, b, c = strsplit(".", ravGPS.version)
+            local d, e, f = strsplit(".", message)
+            if (d > a) or (d == a and e > b) or (d == a and e == b and f > c) then
+                prettyPrint(string.format(ravGPS.locales[ravGPS.locale].load.outofdate, ravGPS.name))
+                RAVGPS_seenUpdate = true
+            end
+        end
+    end
+end
+local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("CHAT_MSG_ADDON")
+f:SetScript("OnEvent", OnEvent)
---
+++ locales.lua
@@ -0,0 +1,30 @@
+local name, ravGPS = ...
+ravGPS.name = "Ravenous GPS"
+ravGPS.version = GetAddOnMetadata(name, "Version")
+ravGPS.github = "https://github.com/waldenp0nd/ravGPS"
+ravGPS.discord = "https://discord.gg/dNfqnRf2fq"
+ravGPS.color = "ff6b6b"
+ravGPS.locales = {
+    ["enUS"] = {
+        ["help"] = {
+            "Information and How to Use",
+            "Type |cff" .. ravGPS.color .. "/%s|r to get your/your target's coordinates.", -- ravGPS.name
+            "You can send your coordinates to different channels, like party, guild, instance, or whisper: |cff" .. ravGPS.color .. "/%s guild|r, |cff" .. ravGPS.color .. "/%s whisper NAME|r.", -- defaults.COMMAND, defaults.COMMAND
+            "Check out |cff" .. ravGPS.color .. "%s|r on GitHub, WoWInterface, or Curse for more info and support!", -- ravGPS.name
+            "You can also get help directly from the author on Discord: %s" -- ravGPS.discord
+        },
+        ["messages"] = {
+            ["player"] = "My coordinates are: ",
+            ["target"] = "%s is up at " -- targetName
+        },
+        ["load"] = {
+            ["outofdate"] = "There is an update available for |cff" .. ravGPS.color .. "%s|r! Please go to GitHub, WoWInterface, or Curse to download.", -- ravGPS.name
+            ["install"] = "Thanks for installing |cff" .. ravGPS.color .. "%s|r!", -- ravGPS.name
+            ["update"] = "Thanks for updating to |cff" .. ravGPS.color .. "v%s|r!", -- ravGPS.version
+            ["both"] = "Type |cff" .. ravGPS.color .. "/%s help|r to familiarize yourself with the addon." -- defaults.COMMAND
+        },
+        ["version"] = "%s is the current version.", -- ravGPS.version
+        ["parentplace"] = "Usually you are unable to place a Map Pin here, but |cff" .. ravGPS.color .. "%s|r has figured out how to place one for you!", -- ravGPS.name
+        ["cantplace"] = "Unable to place a Map Pin here!"
+    }
+}
