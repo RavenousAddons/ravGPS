d8fa328e151b110064fa82f944b2554bf9838b29
waldenp0nd <20667094+waldenp0nd@users.noreply.github.com>
2021-02-07 14:27:45 +0000

    add curse/wowi IDs

--- metadata.lua
+++ metadata.lua
@@ -5,5 +5,4 @@ ns.title = GetAddOnMetadata(name, "Title")
 ns.notes = GetAddOnMetadata(name, "Notes")
 ns.version = GetAddOnMetadata(name, "Version")
 ns.color = "ff6b6b"
-ns.command = "wp"
 ns.discord = "https://discord.gg/9XeTpbcjzu"
